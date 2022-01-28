1852e951a3969868d194c39af48bdfee55466179
AlexHM <alejandro.honrubia@live.u-tad.com>
2022-01-28 20:49:07 +0100

    fixed config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,7 @@
 export default {
   DB: {
-    URL_MONGO:
-      process.env.MONGODB_URL ||
-      "mongodb+srv://adat:1234@cluster0.wkrf7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-
+    URL_MONGO: process.env.MONGODB_URL || "mongodb://localhost/apiChat",
     USER_MONGO: process.env.MONGODB_USER || "",
-
     PASSWORD_MONGO: process.env.MONGODB_PASS || "",
   },
 };
