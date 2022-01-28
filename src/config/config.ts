9c77c3bbb799e7dbba5804587625bb70472ebb7d
AlexHM <alejandro.honrubia@live.u-tad.com>
2022-01-28 20:42:18 +0100

    .

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,9 @@ export default {
     URL_MONGO:
       process.env.MONGODB_URL ||
       "mongodb+srv://adat:1234@cluster0.wkrf7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+
     USER_MONGO: process.env.MONGODB_USER || "",
+
     PASSWORD_MONGO: process.env.MONGODB_PASS || "",
   },
 };
